b395d0e06c5c211bd21f82c53b6f715940eb8745
Tyler Kindy <tylerkindy@gmail.com>
2018-08-12 13:31:13 -0400

    Add buildSrc module

---
+++ buildSrc/src/main/java/com/tylerkindy/github/Dependencies.kt
@@ -0,0 +1,29 @@
+package com.tylerkindy.github
+
+object Versions {
+    val compileSdk = 28
+    val minSdk = 19
+    val targetSdk = 28
+
+    val androidGradle = "3.3.0-alpha05"
+    val kotlin = "1.2.60"
+    val appCompat = "1.0.0-rc01"
+    val constraintLayout = "1.1.2"
+    val junit = "4.12"
+    val testRunner = "1.1.0-alpha4"
+    val espresso = "3.1.0-alpha4"
+}
+
+object Deps {
+    val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradle}"
+
+    val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val testRunner = "androidx.test:runner:${Versions.testRunner}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+
+    val junit = "junit:junit:${Versions.junit}"
+}
