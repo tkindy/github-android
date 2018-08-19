03c0e5d64b1973e70f66dc5f22f76c028c31a191
Tyler Kindy <tylerkindy@gmail.com>
2018-08-19 10:48:56 -0400

    Extract ApolloClient

--- app/src/main/java/com/tylerkindy/github/network/NetworkModule.kt
+++ app/src/main/java/com/tylerkindy/github/network/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.tylerkindy.github.network
 
+import com.apollographql.apollo.ApolloClient
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -10,9 +11,16 @@ class NetworkModule {
 
     @Provides
     @Named(GITHUB_CLIENT)
-    fun provideGitHubClient(tokenInterceptor: TokenInterceptor) =
+    fun provideGitHubClient(tokenInterceptor: TokenInterceptor): OkHttpClient =
             OkHttpClient.Builder()
                     .addInterceptor(tokenInterceptor)
+                    .build()
+
+    @Provides
+    fun provideApolloClient(@Named(GITHUB_CLIENT) githubClient: OkHttpClient): ApolloClient =
+            ApolloClient.builder()
+                    .serverUrl("https://api.github.com/graphql")
+                    .okHttpClient(githubClient)
                     .build()
 
     companion object {
--- app/src/main/java/com/tylerkindy/github/ui/repolist/RepoListViewModel.kt
+++ app/src/main/java/com/tylerkindy/github/ui/repolist/RepoListViewModel.kt
@@ -2,25 +2,18 @@ package com.tylerkindy.github.ui.repolist
 
 import androidx.lifecycle.ViewModel
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.rx2.Rx2Apollo
 import com.tylerkindy.github.RepositoriesQuery
-import com.tylerkindy.github.network.NetworkModule
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
-import okhttp3.OkHttpClient
 import javax.inject.Inject
-import javax.inject.Named
 
 class RepoListViewModel @Inject constructor(
-        @Named(NetworkModule.GITHUB_CLIENT) okHttpClient: OkHttpClient
+        private val apolloClient: ApolloClient
 ) : ViewModel() {
 
-    val apolloClient = ApolloClient.builder()
-            .serverUrl("https://api.github.com/graphql")
-            .okHttpClient(okHttpClient)
-            .build()
-
-    fun getRepos(): Observable<com.apollographql.apollo.api.Response<RepositoriesQuery.Data>> {
+    fun getRepos(): Observable<Response<RepositoriesQuery.Data>> {
         return Rx2Apollo.from(
                 apolloClient.query(
                         RepositoriesQuery.builder().build()
